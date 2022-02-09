2ab91eeed86d8d111f59bb39c14000523dce703d
Diana P <dian6210@stud.kea.dk>
2022-02-09 22:00:26 +0100

    state, reducer, action, useDispatch, useSelector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+  console.log("cash", cash);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cash">{cash}</div>
+      <div className="buttons">
+        <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+      </div>
     </div>
   );
 }
